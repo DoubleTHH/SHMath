b230fe6fa57a6451803f5607a98c115ed55c370d
YangsiHao1997 <13192289459@163.com>
2021-08-13 10:13:04 +0800

    提交定点数数学库

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,22 @@
+﻿using SHMath;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SHVector3 v1 = new SHVector3(Vector3.right);
+        SHVector3 v2 = new SHVector3(Vector3.up);
+        SHArgs value = SHVector3.Angle(v1, v2);
+        Debug.Log("angle  : " + value.ConvertViewAngle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
